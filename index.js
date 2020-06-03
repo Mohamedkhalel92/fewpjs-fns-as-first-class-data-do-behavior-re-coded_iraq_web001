bc8070ce75b792840e3b01951c82bf45ee37dd95
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 14:13:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function handleClick(e) {
 function greet(t){
   const time = t.split(':');
   if(parseInt(time[0],10)<12)return "Good Morning";
-  if(parseInt(time[0],10)>=12 && parseInt(time[0],10)<5 )return "Good Afternoon";
-  if(parseInt(time[0],10)>=5)return "Good Evening";
+  if(parseInt(time[0],10)>=12 && parseInt(time[0],10)<=5 )return "Good Afternoon";
+  if(parseInt(time[0],10)>5)return "Good Evening";
 
 }
 /* Write your implementation of displayMessage() */
