0e771c51daaf0c11d638117d21e652d302bb43cb
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 13:25:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(t){
+  const time = t.split(':');
+  if(time[0].parseInt()<12)return "Good Morning";
+  if(time[0].parseInt()>12 && time[0].parseInt()<5 )return "Good Afternoon";
+  if(time[0].parseInt()>5)return "Good Evening";
+  
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(){
+  
+}
