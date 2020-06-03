0002c17b5608f6dab4e588c1a244a8f8dc34e032
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 13:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 function greet(t){
   const time = t.split(':');
   if(parseInt(time[0],10)<12)return "Good Morning";
-  if(parseInt(time[0],10)>=12 && parseInt(time[0],10)=<5 )return "Good Afternoon";
+  if(parseInt(time[0],10)>12 && parseInt(time[0],10)<5 )return "Good Afternoon";
   if(parseInt(time[0],10)>5)return "Good Evening";
 
 }
